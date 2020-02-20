62ecea7a945d14e757eae8a33c62522b9d3f0c68
David-The-Programmer <davhuzh@gmail.com>
2020-02-20 21:14:24 +0800

    Finished jumping of dino

--- DinoGame/Dino.js
+++ DinoGame/Dino.js
@@ -4,12 +4,18 @@
  * Class for the Dinosaur object
  */
 class Dino {
-    constructor(x, y, w, h, v) {
+    constructor(x, y, w, h, lift) {
         this.x = x;
         this.y = y;
         this.width = w;
         this.height = h;
-        this.velocity = v;
+        this.velocity = 0;
+        this.jumping = false;
+        this.falling = false;
+        this.gameOver = false;
+
+        // amount of force the dino has to lift itself
+        this.lift = lift;
     }
 
     draw() {
@@ -18,16 +24,86 @@ class Dino {
         rect(this.x, this.y, this.width, this.height);
     }
 
+    // updates the y coords of the dino
+    update() {
+        this.y += this.velocity;
+    }
+
     // function for the dino to jump
     // should only jump if the dino is on the ground
-    jump(upwardForce) {
-        this.velocity += upwardForce;
+    // receives
+    jump() {
+        if(this.velocity == 0) {
+            this.velocity = this.lift;
+        } else {
+            this.velocity += 0.05;
+        }
     }
 
     // function for the dino to fall
     // should only fall after reach
+    // receives the downward force
     fall(downwardForce) {
         this.velocity += downwardForce;
+    }
+
+
+    // function to check if the dino has reached the peak point
+    // receives the peak point y coords
+    // returns boolean
+    reachedPeak(yOfPeakPoint) {
+        if (this.y + this.height <= yOfPeakPoint) {
+            this.y = yOfPeakPoint - this.height;
+            return true;
+        }
+    }
+
+    // function to check if the dino is on the ground
+    // receives the ground y coords
+    // returns boolean
+    isOnGround(yOfGround) {
+        if (this.y + this.height >= yOfGround) {
+            this.y = yOfGround - this.height;
+            return true;
+        }
+    }
+
+    // function to trigger jumping
+    triggerJumping() {
+        this.jumping = true;
+    }
+
+    // function for the entire jumping sequence
+    // receives the y coords of ground, y coords of peak point and gravity forces
+    jumpSeq(yOfGround, yOfPeakPoint, gravity) {
+        // if jumping / falling, cannot allow key input from user to take effect
+        // only when on ground, allow key input from user to take effect
+        // only when the dino is in the state of jumping, 
+        // check if dino has reached the peak height from its jump
+        // if it reaches the peak, set dino to be in falling state
+        if (this.jumping) {
+            if (this.reachedPeak(yOfPeakPoint)) {
+                this.jumping = false;
+                this.falling = true;
+                this.velocity = 0;
+            } else {
+                this.jump();
+            }
+        }
+        // only when the dino is in the state of falling,
+        // check if dino has reached the ground
+        // if it reaches the ground, set the dino out of falling state
+        // reset its velocity
+        if (this.falling) {
+            if (this.isOnGround(yOfGround)) {
+                this.falling = false;
+                this.velocity = 0;
+            } else {
+                this.fall(gravity);
+            }
+        }
+        // update the y coords
+        this.update();
     }
 
     // function for collision
@@ -39,8 +115,8 @@ class Dino {
         // (if the y of dino <= y of obstacle + obstacle height ||
         // if the y of dino + dino height >= y of obstacle)
         // If the conditions above are fulfilled, then dino has collided with obstacle
-        if(((this.x <= (obstacle.x + obstacle.width)) || ((this.x + this.width) >= obstacle.x)) 
-        && ((this.y <= (obstacle.y + obstacle.height)) || ((this.y + this.height) >= obstacle.y))) {
+        if (((this.x <= (obstacle.x + obstacle.width)) || ((this.x + this.width) >= obstacle.x))
+            && ((this.y <= (obstacle.y + obstacle.height)) || ((this.y + this.height) >= obstacle.y))) {
             return true;
         }
     }
--- DinoGame/sketch.js
+++ DinoGame/sketch.js
@@ -4,12 +4,89 @@
  * This file is the main program for the game itself only
  */
 
+// Width of the canvas
+const CANVAS_WIDTH = 800;
+
+// Height of the canvas
+const CANVAS_HEIGHT = 300;
+
+// Width of the dino
+const DINO_WIDTH = 36;
+
+// Height of the Dino
+const DINO_HEIGHT = 42;
+
+// width of the dino when ducking
+const DINO_DUCK_WIDTH = 50;
+
+// height of the dino when ducking
+const DINO_DUCK_HEIGHT = 26;
+
+// x position of the dino
+const X_OF_DINO = 100;
+
+// y coordinate of the ground level
+const Y_OF_GROUND = CANVAS_HEIGHT * (3 / 4);
+
+// starting y postion of the dino
+const Y_OF_DINO = Y_OF_GROUND - DINO_HEIGHT;
+
+// y position of the peak (the bottom of the dino has to reach the peak)
+const Y_OF_PEAK = Y_OF_DINO / 1.2;
+
+// starting upward force
+const LIFT = -7;
+
+// downward force
+const GRAVITY = 0.4;
+
+// minimum width of the obstacle
+const MIN_CACTUS_WIDTH = 15;
+
+// minimum HEIGHT of the obstacle
+const MIN_CACTUS_HEIGHT = 30;
+
+// mid width of the obstacle
+const MID_CACTUS_WIDTH = 30;
+
+// mid HEIGHT of the obstacle
+const MID_CACTUS_HEIGHT = 20;
+
+// maximum width of the obstacle
+const MAX_CACTUS_WIDTH = 20;
+
+// maximum HEIGHT of the obstacle
+const MAX_CACTUS_HEIGHT = 40;
+
+const BIRD_WIDTH = 40;
+
+const BIRD_HEIGHT = 34;
+
+// y coordinate of the line above the ground
+const Y_OF_GROUND_LINE = Y_OF_GROUND - 5;
+
+// minimum width between each obstacles
+const MIN_DIST_BTWN_OBS = DINO_WIDTH * 8;
+
+// Dino object
+let dino;
+
 function setup() {
     // init stuff here
-    createCanvas(500, 500);
+    createCanvas(CANVAS_WIDTH, CANVAS_HEIGHT);
+    dino = new Dino(X_OF_DINO, Y_OF_DINO, DINO_WIDTH, DINO_HEIGHT, LIFT);
 }
 
 function draw() {
     // main game loop here
-    background(0); 
+    background(255);
+    dino.jumpSeq(Y_OF_GROUND, Y_OF_PEAK, GRAVITY);
+    dino.draw();
+}
+
+function keyPressed() {
+    // trigger jumping when space is pressed an the dino is on the ground
+    if(key === " " && dino.isOnGround(Y_OF_GROUND)) {
+        dino.triggerJumping();
+    }
 }
